7a602d9c57ba1c64f683f9d3009d26528dbceda8
Joselu <joseluissgz97@gmail.com>
2024-06-22 13:37:03 +0200

    chore: Add NavBar component with styles and functionality

---
+++ src/ui/components/NavBar.tsx
@@ -0,0 +1,38 @@
+import { NavLink, useNavigate } from 'react-router-dom';
+import './NavBar.scss';
+
+export const NavBar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate('/login', {
+      replace: true,
+    });
+  };
+  return (
+    <nav className="nav">
+      <ul className="nav__ul">
+        <li className="nav__li">
+          <NavLink className={({ isActive }) => `nav__link ${isActive ? 'nav__link--active' : ''}`} to="/">
+            Asociaciones
+          </NavLink>
+        </li>
+        <li className="nav__li">
+          <NavLink
+            className={({ isActive }) => `nav__link ${isActive ? 'nav__link--active' : ''}`}
+            to="/marvel"
+          >
+            Marvel
+          </NavLink>
+        </li>
+        <li className="nav__li">
+          <NavLink className={({ isActive }) => `nav__link ${isActive ? 'nav__link--active' : ''}`} to="/dc">
+            DC
+          </NavLink>
+        </li>
+      </ul>
+
+      <button onClick={handleLogout}>Logout</button>
+    </nav>
+  );
+};
