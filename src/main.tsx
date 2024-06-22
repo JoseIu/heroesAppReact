92bc66de6d601deb10e4097264ef25aad069885c
Joselu <joseluissgz97@gmail.com>
2024-06-22 13:36:34 +0200

    update: BrowserRouter and index.scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { HeroesApp } from './HeroesApp.tsx';
-import './index.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HeroesApp />
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
   </React.StrictMode>
 );
