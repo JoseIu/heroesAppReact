a5c455a3e9a824840ae54491afc16bd14d20d248
Joselu <joseluissgz97@gmail.com>
2024-06-23 22:51:40 +0200

    feat: Update AppRouter to include PrivateRoute and PublicRoute

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,13 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth/pages/LoginPage';
 import { HeroRoutes } from '../heroes/routes/HeroRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="/*" element={<HeroRoutes />} />
-        <Route path="/login" element={<LoginPage />} />
+      <Route path="/login" element={<PublicRoute>
+        <LoginPage />
+
+      </PublicRoute>} />
+
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <HeroRoutes />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </>
   );
